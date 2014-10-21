8904d6cb0d83cf695527daab72c93cff6a5fac1a
YinJiaJin <yinjiajin@xiaomi.com>
2014-10-21 16:25:56 +0800

    options package is work

--- example/demo.go
+++ example/demo.go
@@ -5,421 +5,57 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"os"
-	"runtime"
-	//rd "runtime/debug"
-	"strconv"
-	"sync"
-	"time"
 
-	pr "github.com/wheelcomplex/preinit"
+	opt "github.com/wheelcomplex/preinit/options"
+	//l4g "github.com/wheelcomplex/preinit/log4go"
 )
 
-var cpuNum *int     // -c
-var pathStr *string // -p
-var statFlag *bool  // -v
-var debugFlag *bool // -D
-var modFlag *string // -m
-
 func main() {
-	modFlag = flag.String("m", "watch", "run module")
-	pathStr = flag.String("p", "/tmp", "path to scan")
-	cpuNum = flag.Int("c", 1, "use cpu number")
-	statFlag = flag.Bool("v", false, "show stat")
-	debugFlag = flag.Bool("D", false, "show debug")
-
-	flag.Parse()
-	runtime.GOMAXPROCS(*cpuNum)
-
-	if *modFlag == "fmt" {
-		dum := make(map[int]struct{})
-		for i := int(0); i < 1e8; i++ {
-			dum[i] = struct{}{}
-		}
-		println("start to dump")
-		fmt.Printf("dum %v\n", dum)
-		return
-	}
-
-	if *modFlag == "mkdir" {
-		os.Exit(mkdir())
-	}
-	if *modFlag == "mkdir2" {
-		os.Exit(mkdir2())
-	}
-	if *modFlag == "mkdir3" {
-		os.Exit(mkdir3())
-	}
-	if *modFlag == "mkdir4" {
-		os.Exit(mkdir4())
-	}
-	if *modFlag == "mkdir5" {
-		os.Exit(mkdir5())
-	}
-	if *modFlag == "rmdir" {
-		os.Exit(rmdir())
-	}
-
-	if *modFlag == "rmdir3" {
-		os.Exit(rmdir3())
-	}
-
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-	// 8 watcher
-	demo := fswatcher.NewPathWatcher(3)
-	//fmt.Printf("fswatcher.NewWatcher: %v\n", 1024)
-	defer func() {
-		fmt.Printf("demo.defer closing...\n")
-		//time.Sleep(1e9)
-		demo.Close()
-	}()
-
-	demo.SetThreshold(1e5)
-	//demo.SetThreshold(0)
-	//fmt.Printf("demo.SetThreshold(%v)\n", 5)
-	demo.SetMaxCacheIdle(5e9)
-	//fmt.Printf("demo.SetMaxCacheIdle(%v)\n", 8e9)
-
-	eventCh, err := demo.WatchRecursive(*pathStr, true)
-	//fmt.Printf("demo.WatchRecursive(%s) scan end %v, %v ...\n", *pathStr, eventCh, err)
-	if err != nil {
-		fmt.Printf("demo.WatchRecursive(%s) error: %v\n", *pathStr, err)
-		return
-	}
-	fmt.Printf("demo.WatchRecursive(%s) waiting event %v, %v ...\n", *pathStr, eventCh, err)
-	//go func() {
-	//time.Sleep(3e9)
-	//fmt.Println("auto closing")
-	//demo.Close()
-	//}()
-	starts := time.Now()
-	var evcnt int64
-	var everr int64
-	var scancnt int64
-	evMap := make(map[uint64]*fswatcher.ActEvent)
-	if *statFlag {
-		go func() {
-			tk := time.NewTicker(5e9) // one second tick
-			ts := time.Now()
-			defer tk.Stop()
-			var pre, cur uint64
-			for {
-				//runtime.GC()
-				//rd.FreeOSMemory()
-				wc, cache, pending := demo.Stat()
-				ms := &runtime.MemStats{}
-				runtime.ReadMemStats(ms)
-				cur = ms.Alloc + ms.Sys
-				if pre != cur {
-					fmt.Printf("MEM Used %d/Res %d,  watching %d, cache %d, pending %d, event %d(scan %d), error %d, timing %v\n", ms.Alloc/1024/1024, ms.Sys/1024/1024, wc, cache, pending, evcnt, scancnt, everr, ts.Sub(starts))
-					pre = cur
-				}
-				ts = <-tk.C
-			}
-		}()
-	}
-	//for _ = range eventCh {
-	//}
-	for ev := range eventCh {
-		////slow down output
-		//for i := 0; i < 5; i++ {
-		//	misc.UUID()
-		//}
-		event := ev.(*fswatcher.ActEvent)
-		if event.Stage == fswatcher.STAGE_INIT {
-			scancnt++
-		}
-		//evMap[event.Inode] = event
-		if event.Err == nil {
-			if *debugFlag {
-				fmt.Printf("eventCh: %v\n", event)
-			}
-			evcnt++
-		} else {
-			//if *debugFlag {
-			fmt.Printf("eventCh Error: %v\n", event)
-			//}
-			everr++
-			//break
-		}
-	}
-	fmt.Printf("demo.WatchRecursive(%s) end, map %d, event %d, error %d, timing %d\n", *pathStr, len(evMap), evcnt, everr, time.Now().Sub(starts))
-	time.Sleep(1e9)
-	return
-}
-
-func rmdir3() int {
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot3"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	os.Mkdir(rootdir, os.ModePerm)
-	basedir := oldpath + "/" + rootdir + "/"
-	for i := int64(0); i < 1000000; {
-		wg.Add(1)
-		go func(start int64) {
-			for j := int64(0); j < 100000; j++ {
-				dirb := basedir + strconv.Itoa(int(j+start))
-				if err := os.Remove(dirb); err != nil {
-					//fmt.Println("rmdir", dirb, err)
-					//return
-				}
-				//fmt.Println("mkdir", dirb)
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-			}
-			wg.Done()
-		}(i)
-		i = i + 100000
-	}
-	wg.Wait()
-	fmt.Println("rmdir3", rootdir, cnt)
-	return 0
-}
-
-func mkdir3() int {
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot3"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	os.Mkdir(rootdir, os.ModePerm)
-	basedir := oldpath + "/" + rootdir + "/"
-	for i := int64(0); i < 1000000; {
-		wg.Add(1)
-		go func(start int64) {
-			for j := int64(0); j < 100000; j++ {
-				dirb := basedir + strconv.Itoa(int(j+start))
-				if err := os.Mkdir(dirb, os.ModePerm); err != nil {
-					fmt.Println("mkdir", dirb, err)
-					//return
-				}
-				//fmt.Println("mkdir", dirb)
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-			}
-			wg.Done()
-		}(i)
-		i = i + 100000
-	}
-	wg.Wait()
-	fmt.Println("mkdir", rootdir, cnt)
-	return 0
-}
-
-func mkdir2() int {
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot2"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	os.Mkdir(rootdir, os.ModePerm)
-	for i := 0; i < 100; i++ {
-		dira := oldpath + "/" + rootdir + "/" + strconv.Itoa(i)
-		if err := os.Mkdir(dira, os.ModePerm); err != nil {
-			fmt.Println("mkdir", dira, err)
-			return 1
-		}
-		dirmu.Lock()
-		cnt++
-		dirmu.Unlock()
-		wg.Add(1)
-		go func(start string) {
-			for j := 0; j < 100; j++ {
-				dirb := start + "/" + strconv.Itoa(j)
-				if err := os.Mkdir(dirb, os.ModePerm); err != nil {
-					fmt.Println("mkdir", dirb, err)
-					return
-				}
-				//fmt.Println("mkdir", dirb)
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-			}
-			wg.Done()
-		}(dira)
-	}
-	wg.Wait()
-	fmt.Println("mkdir", rootdir, cnt)
-	return 0
-}
-
-func mkdir() int {
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	os.Mkdir(rootdir, os.ModePerm)
-	for i := 0; i < 1000; i++ {
-		dira := oldpath + "/" + rootdir + "/" + strconv.Itoa(i)
-		if err := os.Mkdir(dira, os.ModePerm); err != nil {
-			fmt.Println("mkdir", dira, err)
-			return 1
-		}
-		dirmu.Lock()
-		cnt++
-		dirmu.Unlock()
-		wg.Add(1)
-		go func(start string) {
-			for j := 0; j < 100; j++ {
-				dirb := start + "/" + strconv.Itoa(j)
-				if err := os.Mkdir(dirb, os.ModePerm); err != nil {
-					fmt.Println("mkdir", dirb, err)
-					return
-				}
-				//fmt.Println("mkdir", dirb)
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-			}
-			wg.Done()
-		}(dira)
-	}
-	wg.Wait()
-	fmt.Println("mkdir", rootdir, cnt)
-	return 0
-}
-
-func mkdir4() int {
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	os.Mkdir(rootdir, os.ModePerm)
-	for i := 0; i < 1000; i++ {
-		dira := oldpath + "/" + rootdir + "/" + strconv.Itoa(i)
-		if err := os.Mkdir(dira, os.ModePerm); err != nil {
-			fmt.Println("mkdir", dira, err)
-			return 1
-		}
-		dirmu.Lock()
-		cnt++
-		dirmu.Unlock()
-		wg.Add(1)
-		go func(start string) {
-			for j := 0; j < 1000; j++ {
-				dirb := start + "/" + strconv.Itoa(j)
-				if err := os.Mkdir(dirb, os.ModePerm); err != nil {
-					fmt.Println("mkdir", dirb, err)
-					return
-				}
-				//fmt.Println("mkdir", dirb)
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-			}
-			wg.Done()
-		}(dira)
-	}
-	wg.Wait()
-	fmt.Println("mkdir", rootdir, cnt)
-	return 0
-}
-
-func mkdir5() int {
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	os.Mkdir(rootdir, os.ModePerm)
-	for i := 0; i < 100; i++ {
-		dira := oldpath + "/" + rootdir + "/" + strconv.Itoa(i)
-		if err := os.Mkdir(dira, os.ModePerm); err != nil {
-			//fmt.Println("mkdir", dira, err)
-			//return 1
-		}
-		dirmu.Lock()
-		cnt++
-		dirmu.Unlock()
-		wg.Add(1)
-		go func(start string) {
-			for j := 0; j < 100; j++ {
-				dirb := start + "/" + strconv.Itoa(j)
-				if err := os.Mkdir(dirb, os.ModePerm); err != nil {
-					//fmt.Println("mkdir", dirb, err)
-					//return
-					//continue
-				}
-				//fmt.Println("mkdir", dirb)
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-				wg.Add(1)
-				go func(start string) {
-					for j := 0; j < 100; j++ {
-						dirb := start + "/" + strconv.Itoa(j)
-						if err := os.Mkdir(dirb, os.ModePerm); err != nil {
-							//fmt.Println("mkdir", dirb, err)
-							//return
-							//continue
-						}
-						//fmt.Println("mkdir", dirb)
-						dirmu.Lock()
-						cnt++
-						dirmu.Unlock()
-					}
-					wg.Done()
-				}(dirb)
-			}
-			wg.Done()
-		}(dira)
-	}
-	wg.Wait()
-	fmt.Println("mkdir", rootdir, cnt)
-	return 0
-}
-
-//
-func rmdir() int {
-	runtime.GOMAXPROCS(1)
-	dirmu := sync.Mutex{}
-	rootdir := "watchroot"
-	oldpath, _ := os.Getwd()
-	wg := sync.WaitGroup{}
-	var cnt int64
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		dira := oldpath + "/" + rootdir + "/" + strconv.Itoa(i)
-		go func(start string) {
-			for j := 0; j < 1000; j++ {
-				dirb := start + "/" + strconv.Itoa(j)
-				if err := os.Remove(dirb); err != nil {
-					//fmt.Println("rmdir", dirb, err)
-					continue
-				}
-				dirmu.Lock()
-				cnt++
-				dirmu.Unlock()
-			}
-			wg.Done()
-		}(dira)
-	}
-	wg.Wait()
-	for i := 0; i < 1000; i++ {
-		dira := oldpath + "/" + rootdir + "/" + strconv.Itoa(i)
-		if err := os.Remove(dira); err != nil {
-			//fmt.Println("rmdir", dira, err)
-			continue
-		}
-		dirmu.Lock()
-		cnt++
-		dirmu.Unlock()
-	}
-	fmt.Println("rmdir", rootdir, cnt)
-	return 0
+	//l4g.Trace("Received message: %s (%d)", "aaaaa", 5)
+	//l4g.Debug("Received message: %s (%d)", "aaaaa", 5)
+	//l4g.Error("Received message: %s (%d)", "aaaaa", 5)
+	fmt.Println("\np0--------------------:", opt.String())
+	//
+	cmdline := "-bool1 -bool2 -a a --bl bl -c 0,1,2,3,4,5 f1 f2 f3 f4"
+	//cmdline := "--bh -bj"
+	fmt.Println("---------------- parse:", cmdline)
+	opt.ParseString(cmdline)
+	fmt.Println("p1--------------------:", opt.String())
+	opt.SetParserFlag("--ooo", "out1")
+	opt.SetParserFlag("--ooo", "out2")
+	opt.SetParserFlag("--fds", "19,20")
+	opt.SetParserFlag("--", "-")
+	opt.SetParserFlag("f6", "f7")
+	opt.SetParserFlag("--bh", "-bj")
+	fmt.Println("p2--------------------:", opt.String())
+	opt.DelParserFlag("f7", "")
+	opt.DelParserFlag("-c", "0")
+	opt.DelParserFlag("-a", "0")
+	opt.DelParserFlag("--bl", "")
+	opt.DelParserFlag("-bj", "")
+	fmt.Println("p3--------------------:", opt.String())
+	opt.Usage()
+	opt.SetVersion("java version \"%s\"", "1.7.0_60")
+	opt.SetDescription(`Java(TM) SE Runtime Environment (build 1.7.0_60-b19)
+Java HotSpot(TM) 64-Bit Server VM (build 24.60-b09, mixed mode)`)
+	opt.SetNotes("java is come from oracle")
+	opt.SetOption("--thread", "9", "set max running thread, 0 for all number of CPU")
+	opt.SetOptions("--fds", []string{"6", "7", "8", "9"}, "set file description pass to children")
+	opt.SetFlag("--foreground", "run app in foreground, default is run in background(daemon)")
+	opt.SetFlag("-F", "run app in foreground, default is run in background(daemon)")
+	opt.SetFlag("--", "test --, run app in foreground, default is run in background(daemon)")
+	opt.SetNoFlags([]string{"f11", "f2", "f3", "f14"}, "file list to compress")
+	fmt.Println("p4--------------------:", opt.String())
+	opt.Usage()
+	fmt.Println("p5--------------------")
+	fmt.Println("lists:", opt.GetNoFlagString())
+	fmt.Println("--thread:", opt.GetInt("--thread"))
+	fmt.Println("--fds:", opt.GetInts("--fds"))
+	// no defined option
+	fmt.Println("--nodef:", opt.GetString("--nodef"))
+	fmt.Println("--nodefs:", opt.GetStringList("--nodefs"))
+	fmt.Println("p6--------------------:", opt.String())
 }
 
 //
--- preinit.go
+++ preinit.go
@@ -2,9 +2,9 @@
 
 1. log
 2. args
-3. daemon
+3. daemon+UID/GID
 4. signal
-5. fdpass
+5. fdpass+drop privileges
 6. children monitor
 
 */
@@ -16,10 +16,451 @@
 package preinit
 
 import (
-	"github.com/wheelcomplex/preinit/log4go"
+	//"github.com/wheelcomplex/preinit/log4go"
+	//"fmt"
+	"os"
+	"strings"
 )
 
-//// VARS ////
+//// command line arguments support, flag and usage help ////
+/*
+. less extenal package used(os/fmt/strings)
+. parser struct
+. short + long arguments
+. value and bool
+. no-dash arguments list
+. int, string, bool
+. array supported: --aa 1 --aa 2 --aa 3 => aa = {1,2,3}
+. value return only
+. space between flag and value
+. osargs = pr.Parse(), parse os.Args.String()
+. strargs = pr.StringParse(args string), parse args
+. strargs.GetInt("-d", "--debuglevel", 2, "debug level, 0-7, higher for more debug info"), return one int
+. strargs.GetIntList("-d", "--debuglevel", []int{1, 2, 3}, "debug level, 0-7, higher for more debug info"), return []int
+. strargs.GetNoFlags(), return []string of no-flag options
+. no exit by invalid value
 
-// debug level 0-8, default
+.
+*/
+
+// argsToLine convert []string to string line, split by space
+func argsToLine(list []string) string {
+	return strings.Join(list, " ")
+}
+
+// lineToArgs convert string to []string
+func lineToArgs(line string) []string {
+	return strings.Split(cleanSpaces(line), " ")
+}
+
+// argsIndex return index of flag in args, if no found return -1
+func argsIndex(args []string, flag string) int {
+	var index int = -1
+	for idx, val := range args {
+		if val == flag {
+			index = idx
+			break
+		}
+	}
+	return index
+}
+
+// cleanSpaces
+func cleanSpaces(line string) string {
+	var oldline string
+	for {
+		oldline = line
+		line = strings.Replace(line, "  ", " ", -1)
+		if oldline == line {
+			break
+		}
+	}
+	return strings.Trim(line, " ")
+}
+
+// opt paser struct
+type optParser_t struct {
+	shortKeys  []string            // list of -flag
+	shortArr   map[string][]string // list for '-f' options
+	longKeys   []string            // list of --flag
+	longArr    map[string][]string // list for '--flag' options
+	noFlagList []string            // list for '/path/filename /path/file2 /path/file3'
+	defaults   map[string][]string // defaults value for flags, no-flag default value use key _PARSER_NOFLAG_INDEX_
+}
+
+// NewOptParserString parsed args and return opt paser struct
+func NewOptParserString(line string) *optParser_t {
+	return NewOptParser(lineToArgs(line))
+}
+
+// NewOptParser parsed args and return opt paser struct
+func NewOptParser(args []string) *optParser_t {
+	op := new(optParser_t)
+	op.defaults = make(map[string][]string)
+	op.Parse(args)
+	return op
+}
+
+// reset parser for reuses
+// old value discardeds
+func (op *optParser_t) reset() {
+	op.shortKeys = make([]string, 0, 0)
+	op.shortArr = make(map[string][]string)
+	op.longKeys = make([]string, 0, 0)
+	op.longArr = make(map[string][]string)
+	op.noFlagList = make([]string, 0, 0)
+	// do not reset usage/default
+}
+
+// String convert opt paser struct to strings
+func (op *optParser_t) String() string {
+	var line string
+	// for map
+	for _, k1 := range op.shortKeys {
+		// for slice
+		for _, v2 := range op.shortArr[k1] {
+			line = line + " " + k1 + " " + v2
+		}
+	}
+	// for map
+	for _, k1 := range op.longKeys {
+		// for slice
+		for _, v2 := range op.longArr[k1] {
+			line = line + " " + k1 + " " + v2
+		}
+	}
+	line = line + " " + argsToLine(op.noFlagList)
+	return cleanSpaces(line)
+}
+
+// ParseString get opt paser struct ready to use
+func (op *optParser_t) ParseString(line string) {
+	op.Parse(lineToArgs(line))
+}
+
+// ParseMap get opt paser struct ready to use
+func (op *optParser_t) Parse(args []string) {
+	// reset
+	op.reset()
+	tmpList := make([]string, 0, len(args)+1)
+	tmpList = append(tmpList, args...)
+	tmpList = append(tmpList, "_PARSER_LAST_HOLDER_")
+	// parse
+	var newFlag string
+	var longFlag bool
+	for _, val := range tmpList {
+		//println(longFlag, "newFlag", newFlag, "next", val)
+		if val == "_PARSER_LAST_HOLDER_" {
+			val = ""
+		}
+		if newFlag != "" {
+			if len(val) > 0 && strings.HasPrefix(val, "--") == false && strings.HasPrefix(val, "-") == false {
+				// this is value for newFlags
+				if longFlag {
+					if argsIndex(op.longKeys, newFlag) == -1 {
+						op.longKeys = append(op.longKeys, newFlag)
+					}
+					if _, ok := op.longArr[newFlag]; ok == false {
+						op.longArr[newFlag] = make([]string, 0, 0)
+					}
+					op.longArr[newFlag] = append(op.longArr[newFlag], val)
+				} else {
+					if argsIndex(op.shortKeys, newFlag) == -1 {
+						op.shortKeys = append(op.shortKeys, newFlag)
+					}
+					if _, ok := op.shortArr[newFlag]; ok == false {
+						op.shortArr[newFlag] = make([]string, 0, 0)
+					}
+					op.shortArr[newFlag] = append(op.shortArr[newFlag], val)
+				}
+				newFlag = ""
+				longFlag = false
+				continue
+			} else {
+				// no value for newFlags, it's bool flag
+				if longFlag {
+					if argsIndex(op.longKeys, newFlag) == -1 {
+						op.longKeys = append(op.longKeys, newFlag)
+					}
+					if _, ok := op.longArr[newFlag]; ok == false {
+						op.longArr[newFlag] = make([]string, 0, 0)
+					}
+					op.longArr[newFlag] = append(op.longArr[newFlag], "")
+				} else {
+					if argsIndex(op.shortKeys, newFlag) == -1 {
+						op.shortKeys = append(op.shortKeys, newFlag)
+					}
+					if _, ok := op.shortArr[newFlag]; ok == false {
+						op.shortArr[newFlag] = make([]string, 0, 0)
+					}
+					op.shortArr[newFlag] = append(op.shortArr[newFlag], "")
+				}
+			}
+			newFlag = ""
+			longFlag = false
+		}
+		if len(val) == 0 {
+			continue
+		}
+		switch {
+		case strings.HasPrefix(val, "--"):
+			newFlag = val
+			longFlag = true
+		case strings.HasPrefix(val, "-"):
+			newFlag = val
+			longFlag = false
+		default:
+			op.noFlagList = append(op.noFlagList, val)
+			newFlag = ""
+			longFlag = false
+		}
+	}
+}
+
+// GetNoFlags return no-flag list in []string
+func (op *optParser_t) GetNoFlags() []string {
+	return op.noFlagList
+}
+
+// GetNoFlags return no-flag list in string
+func (op *optParser_t) GetNoFlagString() string {
+	return ArgsToLine(op.GetNoFlags())
+}
+
+// GetString return first value of this keys
+func (op *optParser_t) GetString(key string) string {
+	if key == "" {
+		return ""
+	}
+	if _, ok := op.shortKeys[key]; ok {
+		// return first value of this key
+		return op.shortArr[key][0]
+	}
+	if _, ok := op.longKeys[key]; ok {
+		// return first value of this key
+		return op.longArr[key][0]
+	}
+	return ""
+}
+
+// GetStringList return list value of this keys
+// if no exist, return empty []string
+func (op *optParser_t) GetStringList(key string) []string {
+	if key == "" {
+		return make([]string, 0, 0)
+	}
+	if _, ok := op.shortKeys[key]; ok {
+		// return list value of this key
+		return op.shortArr[key]
+	}
+	if _, ok := op.longKeys[key]; ok {
+		// return list value of this key
+		return op.longArr[key]
+	}
+	return make([]string, 0, 0)
+}
+
+// GetOptNoFlags return no flag list in []string
+// if option no exist, return defval(if no default defined return nil)
+func (op *optParser_t) GetOptNoFlags() []string {
+	val := op.GetNoFlags()
+	if len(val) == 0 {
+		// try defaut value
+		if _, ok := op.defaults["_PARSER_NOFLAG_INDEX_"]; ok {
+			// use first value of this flag
+			val = op.defaults["_PARSER_NOFLAG_INDEX_"]
+		}
+	}
+	return val
+}
+
+// GetOptNoFlagString return no flag list in string
+// if option no exist, return defval(if no default defined return nil)
+func (op *optParser_t) GetOptNoFlagString() string {
+	return argsToLine(op.GetOptNoFlags())
+}
+
+// GetOptString return first value of option
+// if option no exist, return defval(if no default defined return empty)
+func (op *optParser_t) GetOptString(flag string) string {
+	val := op.GetString(flag)
+	if val == "" {
+		// try defaut value
+		if _, ok := op.defaults[flag]; ok {
+			// use first value of this flag
+			val = op.defaults[flag][0]
+		}
+	}
+	return val
+}
+
+// DelFlag modify optParser_t to match commandLine removed "key value"
+// if key is flag, value == "" will remove all value of key, otherwise remove only flag match "key value"
+func (op *optParser_t) DelFlag(key, value string) {
+	newop := NewOptParserString(op.String())
+	delop := NewOptParserString(cleanSpaces(key) + " " + cleanSpaces(value))
+	//println("DelFlag In", key, "||", value, "||", "=>", cleanSpaces(key)+" "+cleanSpaces(value), "||", newop.String())
+	op.reset()
+	// sync short flags
+	var match bool
+	for _, k1 := range newop.shortKeys {
+		if _, ok := delop.shortArr[k1]; ok {
+			match = true
+			if value == "" {
+				// remove this flag
+				continue
+			}
+		} else {
+			match = false
+		}
+		if argsIndex(op.shortKeys, k1) == -1 {
+			// new flag
+			op.shortKeys = append(op.shortKeys, k1)
+		}
+		// overwrite/reset shortArr
+		op.shortArr[k1] = make([]string, 0, 0)
+		for _, v2 := range newop.shortArr[k1] {
+			if match && v2 == value {
+				// remove this key-value
+				continue
+			}
+			op.shortArr[k1] = append(op.shortArr[k1], v2)
+		}
+	}
+	// sync long flags
+	for _, k1 := range newop.longKeys {
+		if _, ok := delop.longArr[k1]; ok {
+			match = true
+			if value == "" {
+				// remove this flag
+				continue
+			}
+		} else {
+			match = false
+		}
+		if argsIndex(op.longKeys, k1) == -1 {
+			// new flag
+			op.longKeys = append(op.longKeys, k1)
+		}
+		// overwrite/reset longArr
+		op.longArr[k1] = make([]string, 0, 0)
+		for _, v2 := range newop.longArr[k1] {
+			if match && v2 == value {
+				// remove this key-value
+				continue
+			}
+			op.longArr[k1] = append(op.longArr[k1], v2)
+		}
+	}
+	// sync standalone value
+	for _, val := range newop.noFlagList {
+		if argsIndex(delop.noFlagList, val) != -1 {
+			// remove this standalone value
+			continue
+		}
+		op.noFlagList = append(op.noFlagList, val)
+	}
+	//println("DelFlag Out", key, "||", value, "||", op.String())
+}
+
+// SetFlag modify optParser_t to match commandLine "key value"
+// empty string will be ignored
+// string will be trimmed befor save to optParser_t
+// if key is flag(start with - or --) old value of this flag will be overwrited
+func (op *optParser_t) SetFlag(key, value string) {
+	newop := NewOptParserString(cleanSpaces(key) + " " + cleanSpaces(value))
+	//println("SetFlagIn", key, "||", value, "||", "=>", cleanSpaces(key)+" "+cleanSpaces(value), "||", newop.String())
+	// sync short flags
+	for _, k1 := range newop.shortKeys {
+		if argsIndex(op.shortKeys, k1) == -1 {
+			// new flag
+			op.shortKeys = append(op.shortKeys, k1)
+		}
+		// overwrite/reset shortArr
+		op.shortArr[k1] = make([]string, 0, 0)
+		for _, v2 := range newop.shortArr[k1] {
+			op.shortArr[k1] = append(op.shortArr[k1], v2)
+		}
+	}
+	// sync long flags
+	for _, k1 := range newop.longKeys {
+		if argsIndex(op.longKeys, k1) == -1 {
+			// new flag
+			op.longKeys = append(op.longKeys, k1)
+		}
+		// overwrite/reset longArr
+		op.longArr[k1] = make([]string, 0, 0)
+		for _, v2 := range newop.longArr[k1] {
+			op.longArr[k1] = append(op.longArr[k1], v2)
+		}
+	}
+	// sync standalone value
+	op.noFlagList = append(op.noFlagList, newop.noFlagList...)
+	//println("SetFlagOut", key, "||", value, "||", op.String())
+}
+
+// copy of os.Args for default arguments parser
+var Args []string
+
+// convert Args([]string) to line string
+var ArgLine string
+
+// default opt Parser
+var Opts *optParser_t
+
+// initial default command line parser
+func args_init() {
+	Args = make([]string, 0, 0)
+	Args = append(Args, os.Args...)
+	// default opt Parser
+	Opts = NewOptParser(Args)
+	ArgLine = Opts.String()
+}
+
+// wrapper of Opts.Parse
+func Parse(args []string) {
+	Opts.Parse(args)
+}
+
+// wrapper of Opts.ParseString
+func ParseString(line string) {
+	Opts.ParseString(line)
+}
+
+func init() {
+	args_init()
+	println("preinit.init() end.")
+}
+
+//// base logging support ////
+/*
+1. multi-logger in one proc
+2. multi-output in one logger
+3. output filter by debug level
+4. output file rotation by size
+4. default logger contorl by commandline args(--debuglevel, --errlogfile, --logfile, --debuglogfile, --logrotation, --logmaxsize)
+5. Drop-in compatibility with code using the standard log package
+
+// debug level come from log4go: Finest(7), Fine(6), Debug(5), Trace(4), Info(3), Warning(2), Error(1), Critical(0)
+
+level <= 2 write to --errlogfile
+
+level <= 3 write to --logfile
+
+level <= 7 write to --debuglogfile
+
+stdout() => Info(3)
+stderr() => Warning(2)
+
+no output format
+
+
+*/
+
+type prelog_t struct {
+	level int // debug level 0-8, default
+}
+
 var debugLevel int
+
+//// VARS ////

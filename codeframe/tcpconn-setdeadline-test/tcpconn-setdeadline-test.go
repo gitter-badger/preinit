e3880174eba1f8cb8116f4c1691aea6b07607d76
YinJiaJin <yinjiajin@xiaomi.com>
2015-01-15 10:14:14 +0800

    sync write and read

--- codeframe/tcpconn-setdeadline-test/tcpconn-setdeadline-test.go
+++ codeframe/tcpconn-setdeadline-test/tcpconn-setdeadline-test.go
@@ -18,31 +18,47 @@ func main() {
 		println(lerr.Error())
 		os.Exit(9)
 	}
+	synch := make(chan struct{}, 1)
 	go func() {
+		synch <- struct{}{}
 		conn, _ := l.AcceptTCP() // *net.TCPConn
 		defer conn.Close()
 		fmt.Printf("new conn %s\n", conn.RemoteAddr().String())
 		buf := []byte("0123456789")
-		tk := time.NewTicker(2e9)
 		for i := 0; i < 10; i++ {
-			conn.Write(buf[i : i+1])
-			fmt.Printf("out#%d: %s\n", i, buf[i:i+1])
-			<-tk.C
+			<-synch
+			_, err := conn.Write(buf[i : i+1])
+			if err != nil {
+				fmt.Printf("out#%d: %s\n", i, err.Error())
+				break
+			} else {
+				fmt.Printf("out#%d: %s\n", i, buf[i:i+1])
+			}
 		}
 	}()
 	defer l.Close()
-	time.Sleep(2e9)
+	// wait for listener
+	<-synch
 	nfd, err := net.Dial("tcp", "127.0.0.1:8800")
 	if err != nil {
 		println(err.Error())
 		os.Exit(9)
 	}
 	defer nfd.Close()
-	limit := time.Now().Add(5e9)
+	limit := time.Now().Add(3e9)
 	nfd.SetDeadline(limit)
 	rbuf := make([]byte, 10)
+	tk := time.NewTicker(1e9)
+	defer tk.Stop()
+	synch <- struct{}{}
 	for i := 0; i < 5; i++ {
 		nr, ne := nfd.Read(rbuf)
-		fmt.Printf("read %d byte, err %v: %v\n", nr, ne, rbuf)
+		fmt.Printf("read %d byte, err %v: %v\n", nr, ne, rbuf[:nr])
+		if ne != nil {
+			break
+		}
+		synch <- struct{}{}
+		<-tk.C
 	}
+	<-tk.C
 }
